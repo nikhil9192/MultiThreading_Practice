15c94c9bfdc46f32f46f12046786c7c7d88e4834
nikhil9192 <nikhilsinghtech2580@gmail.com>
2023-10-24 21:49:09 +0530

    Add project files.

---
+++ MultiThreading_Practice/Multithread.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading_Practice
+{
+    public  class Multithread
+    {
+        public static void fun1()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread are Excutiable the time  : "+i);
+
+                Thread.Sleep(200);
+            }
+        }
+
+        public static void fun2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Thread are Excutiable the time  : " + i*2);
+            }
+        }
+        public static void fun3()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(20);
+                Console.WriteLine("Thread are Excutiable the time  : " + i * 5);
+            }
+        }
+    }
+}
---
+++ MultiThreading_Practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Multithread m = new Multithread();
+         //  Multithread.fun1();
+
+            Thread th1 = new Thread(Multithread.fun1);
+            Thread th2 = new Thread(Multithread.fun2);
+            Thread th3 = new Thread(Multithread.fun3);
+            th1.Start();
+            th2.Start();
+            th3.Start();
+            Console.ReadLine();
+        }
+    }
+}
